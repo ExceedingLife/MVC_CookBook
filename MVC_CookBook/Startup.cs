7cebc45e0ab1a7855ba0b4172f18fefba0a24b74
Andrew Harkins <ExtremelyExceeding@gmail.com>
2019-05-18 17:48:06 -0500

    Add project files.

---
+++ MVC_CookBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_CookBook.Startup))]
+namespace MVC_CookBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
