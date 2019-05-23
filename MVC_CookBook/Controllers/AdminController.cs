d6226266dfc03dbb86008de4c8b0f47da0bacb0e
Andrew Harkins <extremelyexceeding@gmail.com>
2019-05-23 16:28:46 -0500

    added
    at work

---
+++ MVC_CookBook/Controllers/AdminController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MVC_CookBook.Models;
+
+
+namespace MVC_CookBook.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private readonly ApplicationDbContext context;
+
+        public AdminController()
+        {
+            context = new ApplicationDbContext();
+        }
+        public AdminController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            context = new ApplicationDbContext();
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get {  return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>(); }
+            private set { _signInManager = value; }
+        }
+        public ApplicationUserManager UserManager
+        {
+            get {  return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            private set { _userManager = value; }
+        }
+        public IAuthenticationManager AuthManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+
+        // GET: Admin/Dashboard/
+        [HttpGet]
+        public ActionResult Dashboard()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public Task<ActionResult> ViewAllUsers()
+        {
+
+            return View();
+        }
+
+    }
+}
